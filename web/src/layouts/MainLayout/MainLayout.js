7cf455d07998223178986eb2ae2ad6ecd8528c84
Morgan Spencer <morgan@spencercreative.co>
2020-11-12 13:05:56 -0500

    Add template id to main layout

--- web/src/layouts/MainLayout/MainLayout.js
+++ web/src/layouts/MainLayout/MainLayout.js
@@ -1,11 +1,23 @@
+import { useLocation } from '@redwoodjs/router'
 import Header from 'src/components/Header'
 import Footer from 'src/components/Footer'
 
 const MainLayout = ({ children }) => {
+  const { pathname } = useLocation()
+  var pageId =
+    pathname !== '/' ? pathname.replace('/', 'template-') : 'template-home'
+  pageId = pageId.includes('/')
+    ? pageId.split('/')[0]
+    : !pageId.includes('template-')
+    ? pageId.replace('-', '')
+    : pageId
+
   return (
     <>
       <Header />
-      <main className="px-wrap flex-1 py-8 md:py-12">{children}</main>
+      <main id={pageId} className="px-wrap flex-1 py-8 md:py-12">
+        {children}
+      </main>
       <Footer />
     </>
   )
