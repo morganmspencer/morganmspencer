39bd20a5ed0bd37167e52b5d30abe00423922262
Morgan Spencer <morgan@spencercreative.co>
2020-11-12 13:06:13 -0500

    Add post cell to post page

--- web/src/pages/PostPage/PostPage.js
+++ web/src/pages/PostPage/PostPage.js
@@ -5,14 +5,6 @@ import PostCell from 'src/components/PostCell'
 const PostPage = ({ slug }) => {
   return (
     <MainLayout>
-      <h1>PostPage</h1>
-      <p>
-        Find me in <code>./web/src/pages/PostPage/PostPage.js</code>
-      </p>
-      <p>
-        My default route is named <code>post</code>, link to me with `
-        <Link to={routes.post()}>Post</Link>`
-      </p>
       <PostCell slug={slug} />
     </MainLayout>
   )
