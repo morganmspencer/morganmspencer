31d6d2a19d00f3d1b0ff74642130a3957c24aeae
Morgan Spencer <morgan@spencercreative.co>
2020-11-12 13:05:38 -0500

    Update post cell

--- web/src/components/PostCell/PostCell.js
+++ web/src/components/PostCell/PostCell.js
@@ -1,6 +1,6 @@
 export const QUERY = gql`
-  query PostQuery({$slug: String!}) {
-    post(slug: $slug)
+  query PostQuery($slug: String!) {
+    postBySlug(slug: $slug)
   }
 `
 
@@ -10,6 +10,18 @@ export const Empty = () => <div>Empty</div>
 
 export const Failure = ({ error }) => <div>Error: {error.message}</div>
 
-export const Success = ({ post }) => {
-  return JSON.stringify(post)
+export const Success = ({ postBySlug }) => {
+  const post = JSON.parse(postBySlug)[0]
+  return (
+    <>
+      {console.log(post)}
+      <article className={`post-${post.id}`}>
+        <header>
+          <h1 dangerouslySetInnerHTML={{ __html: post.title.rendered }} />
+          <img src={post.jetpack_featured_media_url} />
+        </header>
+        <div dangerouslySetInnerHTML={{ __html: post.content.rendered }} />
+      </article>
+    </>
+  )
 }
